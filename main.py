186a4027cdac3f9ba67783c316e50dd2c3b4ad86
ZhoPaul <1206172050@qq.com>
2019-04-05 22:38:40 +0800

    #&#20190405 增加飞机的碰撞检测

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def key_control(hero_temp):
         elif event.type == BULLETEVENT:
             hero_temp.fire() # fire bullet by timer per 250ms
             # print('key_control')
-    
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         hero_temp.move_left()
@@ -42,7 +42,7 @@ def button_control(screen):
     exit_rect = MyRect(red, 280, 480, 130, 60)
     start_text = Mytext("START", 135, 510, height = 35, color = white)
     exit_text = Mytext("EXIT", 345, 510, height = 35, color = white)
-    
+
     # button color and function
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
@@ -51,19 +51,35 @@ def button_control(screen):
         start_text.color = black
         if (click[0] == 1): # start game when click start button
             return False
-      
+
     if (280<=mouse[0]<=410 and 480<=mouse[1]<=540):
         exit_rect.color = bright_red
         exit_text.color = black
         if (click[0] == 1): # exit game when click exit button
             pygame.quit()
             sys.exit()
-    
+
     # draw button and text      
     start_rect.draw_button(screen)
     exit_rect.draw_button(screen)
     start_text.draw_text(screen)
     exit_text.draw_text(screen)
+
+def check_collide(plane_Tmp, block_Tmp):
+    if plane_Tmp.rect.colliderect(block_Tmp.rect):
+        #print('collide')
+        return True
+    return False
+    '''
+    if (block_Tmp.pos_y>=plane_Tmp.pos_y and block_Tmp.pos_y <=plane_Tmp.pos_y + plane_Tmp.height)\
+        or (block_Tmp.pos_y+block_Tmp.height>=plane_Tmp.pos_y and block_Tmp.pos_y+block_Tmp.height <=plane_Tmp.pos_y + plane_Tmp.height):
+        if (block_Tmp.pos_x>=plane_Tmp.pos_x and block_Tmp.pos_x <=plane_Tmp.pos_x + plane_Tmp.width)\
+            or (block_Tmp.pos_x+block_Tmp.width>=plane_Tmp.pos_x and block_Tmp.pos_x+block_Tmp.width <=plane_Tmp.pos_y + plane_Tmp.width):
+            print('collide')
+            return True
+    #print('Not collide')
+    return False
+    '''
 
 def start_game_info():
     screen = pygame.display.set_mode((window_width, window_height), 0, 32)
@@ -73,14 +89,14 @@ def start_game_info():
     text_welcome2 = Mytext("Plane War!", window_width/2, window_height/2, height = 50, color = white)
     
     game_info = True
-    
+
     while game_info:
         screen.blit(backgroud, (0, 0))
         text_welcome1.draw_text(screen)
         text_welcome2.draw_text(screen)
         if False == button_control(screen):
             game_info = False
-        
+
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -91,44 +107,30 @@ def start_game_info():
                     game_info = False
                     print(game_info)
                     break
-    
+
 def game_loop():
     game_on = True
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((480, 680), 0, 32)
     background = pygame.image.load('picture/background.png')
     myplane = plane.MyPlane(screen)
-    
     # 绘制障碍物
-    enemy = plane.Enemy(screen)
-    y = 0
-    
+    block = plane.Block(screen)
+
     while game_on:
         screen.blit(background, (0, 0))
-        '''
-        for event in pygame.event.get():
-            if event.type == BULLETEVENT:
-                myplane.fire()
-            elif event.type == pygame.QUIT:
-                print('game on exit')
-                pygame.quit()
-                sys.exit()
-        '''
         key_control(myplane)
         #print("tick is %d", pygame.time.get_ticks())
-        
+
         myplane.display()
-        enemy.display()
-        if y>=0:
-            y=y-5
-        else :
-            y = 680
-        
+        block.display()
         #pygame.draw.rect(screen, random.choice(mycolor), (random.randint(0,480), y,60,50))
+
+        #print(myplane.rect.x)
         
-        
+        check_collide(myplane, block)
         pygame.display.update()
-        clock.tick(60) # 控制游戏运行帧率，没秒60帧
+        clock.tick(60) # 控制游戏运行帧率，每秒60帧
 
 def main():
     pygame.init()
@@ -138,5 +140,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
-    
+
--- src/plane.py
+++ src/plane.py
@@ -8,11 +8,11 @@ class MyPlane(object):
     def __init__(self, screen):
         self.width = 100
         self.height = 124
-        self.pos_x = 190
-        self.pos_y = 520
         self.screen = screen
         self.image = pygame.image.load('picture/hero.gif')
         self.rect = self.image.get_rect()
+        self.rect.x = self.screen.get_width()/2 - self.rect.width/2
+        self.rect.y = self.screen.get_height() - self.rect.height*1.5
         self.speed = 5
         self.bulletNum = 0
         self.bullet_list = []
@@ -21,71 +21,75 @@ class MyPlane(object):
         # 显示子弹
         for bullet in self.bullet_list:
             #print('bullet.pos_y is %d', bullet.pos_y)
-            if bullet.pos_y < 0:
+            if bullet.rect.y < 0:
                 self.bullet_list.remove(bullet)
                 self.bulletNum = self.bulletNum-1
                 continue
             bullet.display()
         # 显示飞机
-        self.screen.blit(self.image, (self.pos_x, self.pos_y))
+        #self.screen.blit(self.image, (self.pos_x, self.pos_y))
+        self.screen.blit(self.image, (self.rect.x, self.rect.y))
         
     def move_left(self):
-        if self.pos_x >= 0 :
-            self.pos_x -= self.speed
+        if self.rect.x >= 0 :
+            self.rect.x -= self.speed
         
     def move_right(self):
-        if self.pos_x <= 385 :
-            self.pos_x += self.speed
+        if self.rect.x <= self.screen.get_width()-self.rect.width:
+            self.rect.x += self.speed
             
     def move_up(self):
-        if self.pos_y >= 0 :
-            self.pos_y -= self.speed
+        if self.rect.y >= 0 :
+            self.rect.y -= self.speed
         
     def move_down(self):
-        if self.pos_y <= 555 :
-            self.pos_y += self.speed
+        if self.rect.y <= self.screen.get_height() - self.rect.height:
+            self.rect.y += self.speed
     
     def fire(self):
         #print('BULLETEVENT')
         if self.bulletNum < 5:
-            temp_bullet = Bullet(self.screen, self.pos_x, self.pos_y)
+            temp_bullet = Bullet(self.screen, self.rect.x, self.rect.y)
             self.bullet_list.append(temp_bullet)
             self.bulletNum = self.bulletNum+1
-        #print('self.bulletNum is %d', self.bulletNum)
+        #print('self.bulletNum is %d', self.bullet_list.__len__())
             
 class Bullet(object):
     def __init__(self, screen, pos_x, pos_y):
         self.image = pygame.image.load('picture/bullet_1.gif')
         self.screen = screen
-        self.pos_x = pos_x
-        self.pos_y = pos_y
+        self.rect = self.image.get_rect()
+        self.rect.x = pos_x
+        self.rect.y = pos_y
         self.speed = 6
 
     def display(self):
-        if self.pos_y >= 0:
-            self.pos_y = self.pos_y - self.speed
+        if self.rect.y >= 0:
+            self.rect.y = self.rect.y - self.speed
         
-        self.screen.blit(self.image, (self.pos_x+48, self.pos_y))
+        self.screen.blit(self.image, (self.rect.x+48, self.rect.y))
             
-class Enemy(object):
-    def __init__(self, screen, pos_x=0, pos_y=0):
+class Block(object):
+    def __init__(self, screen, pos_x=random.randint(0, 450), pos_y=0):
         self.screen = screen
         self.pos_x = pos_x
         self.pos_y = pos_y
+        self.width = 60
+        self.height = 50
         self.speed = 4
         self.color = random.choice(mycolor)
-        #self.rect = (self.pos_x, self.pos_y, 60, 50)
+        self.rect = pygame.Rect(self.pos_x, self.pos_y, self.width, self.height)
 
     def display(self):
         #print("y and x:",self.pos_y,self.pos_x)
-        if self.pos_y >= 0 and self.pos_y <= 680:
-            self.pos_y = self.pos_y + self.speed
-        else :
-            self.pos_y = 0
-            self.pos_x = random.randint(0, 450)
+        if self.rect.y >= 0 and self.rect.y <= 680:
+            self.rect.y = self.rect.y + self.speed
+        else:
+            self.rect.y = 0
+            self.rect.x = random.randint(0, 450)
             self.color = random.choice(mycolor)
-        tmprect = (self.pos_x, self.pos_y, 60, 50)
-        pygame.draw.rect(self.screen, self.color, tmprect)
+        #tmprect = (self.pos_x, self.pos_y, self.width, self.height)
+        pygame.draw.rect(self.screen, self.color, self.rect)
         
         #显示彩色的砖块
         #pygame.draw.rect(self.screen, random.choice(mycolor), tmprect)
